0e636a4674899ee7b0eb8151425d5c7ddf541b62
JerCoolDude <jerte@JaPC>
2023-07-20 21:45:12 -0400

    Added OCExp and procedures

---
+++ src/main/java/net/mcreator/ourcraft_legacy/procedures/CommandGetExpProcedure.java
@@ -0,0 +1,37 @@
+package net.mcreator.ourcraft_legacy.procedures;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.CommandSourceStack;
+
+import net.mcreator.ourcraft_legacy.network.OurcraftLegacyModVariables;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.context.CommandContext;
+
+public class CommandGetExpProcedure {
+	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
+		if (entity == null)
+			return;
+		try {
+			for (Entity entityiterator : EntityArgument.getEntities(arguments, "target")) {
+				if ((new Object() {
+					public Entity getEntity() {
+						try {
+							return EntityArgument.getEntity(arguments, "target");
+						} catch (CommandSyntaxException e) {
+							e.printStackTrace();
+							return null;
+						}
+					}
+				}.getEntity()) instanceof Player _player && !_player.level.isClientSide())
+					_player.displayClientMessage(Component.literal(((entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).level + ""
+							+ (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).exp)), false);
+			}
+		} catch (CommandSyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+}
