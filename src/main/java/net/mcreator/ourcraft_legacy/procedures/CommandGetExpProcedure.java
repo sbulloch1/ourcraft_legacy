866a4ec15dbab1d2002d1b5f5534c898423d2a46
JerCoolDude <jerte@JaPC>
2023-07-20 22:15:34 -0400

    Updated EXP Commands

--- src/main/java/net/mcreator/ourcraft_legacy/procedures/CommandGetExpProcedure.java
+++ src/main/java/net/mcreator/ourcraft_legacy/procedures/CommandGetExpProcedure.java
@@ -1,8 +1,15 @@
 package net.mcreator.ourcraft_legacy.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.CommandSourceStack;
 
-import javax.annotation.Nullable;
+import net.mcreator.ourcraft_legacy.network.OurcraftLegacyModVariables;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.context.CommandContext;
 
 public class CommandGetExpProcedure {
 	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
@@ -20,8 +27,17 @@ public class CommandGetExpProcedure {
 						}
 					}
 				}.getEntity()) instanceof Player _player && !_player.level.isClientSide())
-					_player.displayClientMessage(Component.literal(((entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).level + ""
-							+ (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).exp)), false);
+					_player.displayClientMessage(Component.literal((new Object() {
+						public Entity getEntity() {
+							try {
+								return EntityArgument.getEntity(arguments, "target");
+							} catch (CommandSyntaxException e) {
+								e.printStackTrace();
+								return null;
+							}
+						}
+					}.getEntity() + " has " + (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).exp + " exp and is level "
+							+ (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).level)), false);
 			}
 		} catch (CommandSyntaxException e) {
 			e.printStackTrace();
--- src/main/java/net/mcreator/ourcraft_legacy/procedures/CommandGiveExpProcedure.java
+++ src/main/java/net/mcreator/ourcraft_legacy/procedures/CommandGiveExpProcedure.java
@@ -1,8 +1,17 @@
 package net.mcreator.ourcraft_legacy.procedures;
 
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.CommandSourceStack;
 
-import javax.annotation.Nullable;
+import net.mcreator.ourcraft_legacy.network.OurcraftLegacyModVariables;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.arguments.DoubleArgumentType;
 
 public class CommandGiveExpProcedure {
 	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
@@ -10,7 +19,7 @@ public class CommandGiveExpProcedure {
 			return;
 		if ((StringArgumentType.getString(arguments, "action")).equals("give")) {
 			{
-				double _setval = (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).exp + DoubleArgumentType.getDouble(arguments, "exp");
+				double _setval = (entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).orElse(new OurcraftLegacyModVariables.PlayerVariables())).exp + DoubleArgumentType.getDouble(arguments, "amount");
 				entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
 					capability.exp = _setval;
 					capability.syncPlayerVariables(entity);
@@ -35,11 +44,11 @@ public class CommandGiveExpProcedure {
 							return null;
 						}
 					}
-				}.getEntity() + " has gained " + DoubleArgumentType.getDouble(arguments, "exp") + " exp!")), false);
+				}.getEntity() + " has gained " + DoubleArgumentType.getDouble(arguments, "amount") + " exp!")), false);
 		}
 		if ((StringArgumentType.getString(arguments, "action")).equals("set")) {
 			{
-				double _setval = DoubleArgumentType.getDouble(arguments, "exp");
+				double _setval = DoubleArgumentType.getDouble(arguments, "amount");
 				entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
 					capability.exp = _setval;
 					capability.syncPlayerVariables(entity);
@@ -64,7 +73,36 @@ public class CommandGiveExpProcedure {
 							return null;
 						}
 					}
-				}.getEntity() + "'s exp has been set to " + DoubleArgumentType.getDouble(arguments, "exp") + "!")), false);
+				}.getEntity() + "'s exp has been set to " + DoubleArgumentType.getDouble(arguments, "amount") + "!")), false);
+		}
+		if ((StringArgumentType.getString(arguments, "action")).equals("setlevel")) {
+			{
+				double _setval = DoubleArgumentType.getDouble(arguments, "amount");
+				entity.getCapability(OurcraftLegacyModVariables.PLAYER_VARIABLES_CAPABILITY, null).ifPresent(capability -> {
+					capability.level = _setval;
+					capability.syncPlayerVariables(entity);
+				});
+			}
+			if ((new Object() {
+				public Entity getEntity() {
+					try {
+						return EntityArgument.getEntity(arguments, "target");
+					} catch (CommandSyntaxException e) {
+						e.printStackTrace();
+						return null;
+					}
+				}
+			}.getEntity()) instanceof Player _player && !_player.level.isClientSide())
+				_player.displayClientMessage(Component.literal((new Object() {
+					public Entity getEntity() {
+						try {
+							return EntityArgument.getEntity(arguments, "target");
+						} catch (CommandSyntaxException e) {
+							e.printStackTrace();
+							return null;
+						}
+					}
+				}.getEntity() + "'s level has been set to " + DoubleArgumentType.getDouble(arguments, "amount") + "!")), false);
 		}
 	}
 }
